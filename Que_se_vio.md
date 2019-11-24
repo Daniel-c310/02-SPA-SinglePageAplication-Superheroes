2b1d71578838301bda7947af0fd7fdbf2bbdd7a9
Daniel-c310 <daeddy20@gmail.com>
2019-11-24 03:35:13 -0500

    Ang. carp4 vid13 VerQueSeVio RutasConParametros Router RutasID BotonyA

---
+++ Que_se_vio.md
@@ -0,0 +1,5 @@
+- animacion pagina heroes
+- creacion del component heroe "heroe.component" se va a encargar de mostrar mas informacio de cada heroe
+- Creacion de nuevas rutas en el archivo de rutas CON ID
+uso de RouterLink y creacion de index para "A"
+-BOTON: Libreria Router y creacion de funcion de rutas "verHeroe()"
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../servicios/heroes.service'; //IMPORTACION DE HEROES SERVICIOS DE LA CARPETA SERVICIOS/HEROES
+import { Router } from '@angular/router'; //BOTON: Importamos libreria router para poder cambiarnos de rutas e implementar el sistema de rutas
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +10,21 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = []; //creo una variable donde van a estar todo el arregalo de los heroes, esta variable se va aUTILIZAR EN EL HTML CON EL ngFor y tiene toda TODA la infor de los heroes
 
-  constructor( private _heroesService:HeroesService) { // aqui llamo a la data de los heroes, private _heroesService:HeroesService uso esto para llamar a mi servicio, osea el que esta en la carpeta servicios/heroes. La paabra private es para que se acceda solo dentro de ete servicio
+  constructor( private _heroesService:HeroesService, // aqui llamo a la data de los heroes, private _heroesService:HeroesService uso esto para llamar a mi servicio, osea el que esta en la carpeta servicios/heroes. La paabra private es para que se acceda solo dentro de ete servicio
+               private router:Router) { // BOTON: como se importo el servicio Router, se crea una variable de ese tipo de servicio en el constructor
 
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes(); //igualo el arreglo creado al arreglo que se eimporto con todo el arreglo de heroes, ver que llamo al metodo get que se creo en Servicios/heroes.service
     console.log(this.heroes);
+
+  }
+
+// BOTON: Linea de abajo. se crea una funcion que reciba el index del heroe
+  verHeroe( idx:number ){
+
+    this.router.navigate( ['/heroe',idx] );
+    console.log(idx);
   }
 }
